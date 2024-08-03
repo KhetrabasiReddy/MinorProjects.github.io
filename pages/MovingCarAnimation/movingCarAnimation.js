1bd8eb4187c07ed48e3835aa4c33f515e2b0521d
KhetrabasiReddy <reddykhetrabasi@gmail.com>
2024-08-03 18:07:22 +0530

    Completed the carMovingAnimation Project.

--- pages/MovingCarAnimation/movingCarAnimation.js
+++ pages/MovingCarAnimation/movingCarAnimation.js
@@ -1,1 +1,8 @@
-console.log('initializing script');
+document.addEventListener('click', function() {
+    let audio = document.createElement('audio');
+    audio.setAttribute('src', 'sound.mp3');
+    audio.loop = true;
+    audio.play().catch(error => {
+        console.error('Error playing audio:', error);
+    });
+});
